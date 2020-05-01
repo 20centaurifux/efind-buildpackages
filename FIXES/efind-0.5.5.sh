8ac1ab5e8b69d4ee98b027ace1ba04e770dd23ee
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2020-05-01 16:21:30 +0200

    updated openSUSE Tumbleweed fix

--- FIXES/efind-0.5.5.sh
+++ FIXES/efind-0.5.5.sh
@@ -16,4 +16,11 @@ if [ -x /usr/bin/lsb_release ]; then
 		sed "s/libffi/libffi7/" efind-0.5.5/efind.spec -i
 		sed "s/libffi7-devel/libffi-devel/" efind-0.5.5/efind.spec -i
 	fi
+
+	lsb_release -a | grep -q Tumbleweed
+
+	if [ $? -eq 0 ]; then
+		echo "Updating dependencies for OpenSUSE Tumbleweed"
+		sed "s/libpython3_6m1_0/libpython3_7m1_0/" efind-0.5.5/efind.spec -i
+	fi
 fi
