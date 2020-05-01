b70a1b61231d955658154b022c8eeb538a4c22f5
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2020-05-01 16:11:44 +0200

    updated openSUSE fix

--- FIXES/efind-0.5.5.sh
+++ FIXES/efind-0.5.5.sh
@@ -7,4 +7,13 @@ if [ -x /usr/bin/lsb_release ]; then
 		echo "Updating dependencies for Ubuntu 20.04"
 		sed "s/libffi6/libffi7/" efind-0.5.5/debian/control -i
 	fi
+
+	lsb_release -a | grep -q SUSE
+
+	if [ $? -eq 0 ]; then
+		echo "Updating dependencies for OpenSUSE"
+		sed "s/python3-libs/libpython3_6m1_0/" efind-0.5.5/efind.spec -i
+		sed "s/libffi/libffi7/" efind-0.5.5/efind.spec -i
+		sed "s/libffi7-devel/libffi-devel/" efind-0.5.5/efind.spec -i
+	fi
 fi
